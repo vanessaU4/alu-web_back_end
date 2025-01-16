700b351cf2284c9f3512b0ae27ea95483d142440
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2025-01-16 09:53:40 +0000

    all

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,10 +3,10 @@
 
 
 def index_range(page, page_size):
-    """Return a simple helper function caontaining start and end index"""
-    if (page == 1):
-        return (0, page_size)
+    """Return a tuple containing the start and end index for pagination."""
+    if page == 1:
+        return 0, page_size
     start = (page - 1) * page_size
     end = start + page_size
-    return (start, end)
-    
+    return start, end
+
