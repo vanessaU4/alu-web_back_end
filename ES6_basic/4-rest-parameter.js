a700f777709d0a17ff8830a0e09b9aa7a356d899
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2025-03-02 21:02:29 +0000

    first commit

---
+++ ES6_basic/4-rest-parameter.js
@@ -0,0 +1,9 @@
+// This function returns the number of arguments passed to it
+export default function returnHowManyArguments(...args) {
+    return args.length; // Return the length of the args array
+  }
+  
+  // Example usage of the function
+  console.log(returnHowManyArguments(1, 2, 3)); // Output: 3
+  console.log(returnHowManyArguments('a', 'b')); // Output: 2
+  console.log(returnHowManyArguments()); // Output: 0
