f4f9b664d67bd6bfdc4ac78b2c58a67b7ff114fd
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2025-03-09 20:26:43 +0000

    all

---
+++ ES6_classes/nodesource_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Discussion, issues and change requests at:
+#   https://github.com/nodesource/distributions
+#
+# Script to install the NodeSource Node.js 12.x repo onto a
+# Debian or Ubuntu system.
+#
+# Run as root or insert `sudo -E` before `bash`:
+#
+# curl -sL https://deb.nodesource.com/setup_12.x | bash -
+#   or
+# wget -qO- https://deb.nodesource.com/setup_12.x | bash -
+#
+# CONTRIBUTIONS TO THIS SCRIPT
+#
+# This script is built from a template in
+# https://github.com/nodesource/distributions/tree/master/deb/src
+# please don't submit pull requests against the built scripts.
+#
+
