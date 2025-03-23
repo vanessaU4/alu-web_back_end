29f05e6f6849373dfc6e499dcd6f66a81878c5c8
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2025-03-23 19:48:57 +0000

    queuing system in js

---
+++ queuing_system_in_js/6-processor.js
@@ -0,0 +1,17 @@
+const queue = createQueue({
+    redis: {
+      createClientFactory: () => createClient(),
+    },
+  });
+  
+  function sendNotification(phoneNumber, message) {
+    console.log(
+      `Sending notification to ${phoneNumber}, with message: ${message}`
+    );
+  }
+  
+  queue.process('push_notification_code', (job, done) => {
+    sendNotification(job.data.phoneNumber, job.data.message);
+    done();
+  });
+  
